66afb56ddf08e1af5ecdd46afedc168c27aaefac
Gabriel Poça <gabrielpoca@gmail.com>
2015-01-07 23:41:30 +0000

    Moves everything into lib folder. Asks for key and token.

--- init.js
+++
@@ -1,7 +0,0 @@
-var ignoreSettingsFile = require('./tasks/ignore_settings_file');
-var createSettingsFile = require('./tasks/create_settings_file');
-
-module.exports = function() {
-  createSettingsFile();
-  ignoreSettingsFile();
-}
